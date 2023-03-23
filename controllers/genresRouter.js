ec25a63fcf5eb9b6ab25ec068330c4facb9d65bf
ddoyediran <ddoyediran@gmail.com>
2023-03-23 20:22:09 +0100

    Clean up

--- controllers/genresRouter.js
+++ controllers/genresRouter.js
@@ -19,27 +19,10 @@ function validate(inputObj) {
   return value;
 }
 
-// store genre type
-const genreList = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Adventure" },
-  { id: 3, name: "Comedy" },
-  { id: 4, name: "Crime and mystery" },
-  { id: 5, name: "Fantasy" },
-  { id: 6, name: "History" },
-  { id: 7, name: "Science fiction" },
-];
-
 /// Creating the routes ///
 
 // GET all movies genres in the genre list
 genreRoutes.get("/", async (req, res) => {
-  // if (genreList.length === 0) {
-  //   return res.status(200).json({ message: "Genres list is empty" });
-  // }
-
-  // res.status(200).json({ list: genreList });
-
   try {
     const allGenres = await Genre.find({});
     res.status(200).json({ allGenres });
@@ -61,16 +44,6 @@ genreRoutes.get("/:id", async (req, res) => {
   } catch (err) {
     console.error(err);
   }
-
-  // const genre = genreList.find((genre) => {
-  //   return genre.id === parseInt(req.params.id);
-  // });
-
-  // if (!genre) {
-  //   return res.status(404).json({ message: "Genre not in the Genre List" });
-  // }
-
-  // res.status(200).json({ genre });
 });
 
 // POST: Add a genre to the genre list
@@ -97,20 +70,6 @@ genreRoutes.post("/", async (req, res) => {
   } catch (err) {
     console.error(err);
   }
-
-  // const result = validate({ name: req.body.name });
-  // // if error
-  // if (result.error) {
-  //   // console.log(resultVal.error.details[0].message);
-  //   return res.status(400).send(result.error.details[0].message);
-  // }
-
-  // // no error, then add to the genre array
-  // const newGenre = { id: genreList.length + 1, ...result.value };
-
-  // genreList.push(newGenre);
-
-  // res.status(201).json({ message: "A new genre has been added" });
 });
 
 // PUT: Update existing genre in the genre list
@@ -137,34 +96,6 @@ genreRoutes.put("/:id", async (req, res) => {
   } catch (err) {
     console.error(err);
   }
-
-  // let genreFound = genreList.find((genre) => {
-  //   return genre.id === parseInt(req.params.id);
-  // });
-
-  // // if genre not in the genreList
-  // if (!genreFound) {
-  //   return res.status(404).json({ message: "Genre not found" });
-  // }
-
-  // const result = validate({ name: req.body.name });
-
-  // // if error
-  // if (result.error) {
-  //   // console.log(resultVal.error.details[0].message);
-  //   return res.status(400).send(result.error.details[0].message);
-  // }
-
-  // let updated = {
-  //   id: genreFound.id,
-  //   name: req.body.name,
-  // };
-
-  // const targetIndex = genreList.indexOf(genreFound);
-
-  // genreList.splice(targetIndex, 1, updated);
-
-  // res.status(200).json({ message: updated });
 });
 
 // DELETE: Delete a genre from the genre list
@@ -176,19 +107,6 @@ genreRoutes.delete("/:id", async (req, res) => {
   } catch (err) {
     console.error(err);
   }
-
-  // const genreIndex = genreList.findIndex((genre) => {
-  //   return genre.id === parseInt(req.params.id);
-  // });
-
-  // // if the genre doesn't exist inside the genreList then we don't want to go ahead with performing the delete operation
-  // if (genreIndex < 0) {
-  //   return res.status(404).json({ message: "Can't find genre in the list" });
-  // }
-
-  // genreList.splice(genreIndex, 1); // delete the genre
-
-  // res.status(200).json({ message: "Genre successfully deleted!" });
 });
 
 module.exports = genreRoutes;
