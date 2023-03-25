e0b59c666fc540a2c682953e7ffe6df0bf6dcf60
ddoyediran <ddoyediran@gmail.com>
2023-03-25 17:41:31 +0100

    create genre route updated

--- src/controllers/genresRouter.js
+++ src/controllers/genresRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const Joi = require("joi");
-//const Genre = require("../models/genre");
 const Genre = require("../models/genre");
 
 const genreRoutes = express.Router();
@@ -71,7 +70,7 @@ const getSingleGenre = async (req, res, next) => {
 // });
 
 // POST: Add a genre to the genre list
-genreRoutes.post("/", async (req, res) => {
+const createGenre = async (req, res, next) => {
   try {
     const result = validate({ name: req.body.name });
     // if validation error
@@ -92,9 +91,34 @@ genreRoutes.post("/", async (req, res) => {
 
     res.status(201).json(savedGenre);
   } catch (err) {
-    console.error(err);
+    next(err);
   }
-});
+};
+
+// genreRoutes.post("/", async (req, res) => {
+//   try {
+//     const result = validate({ name: req.body.name });
+//     // if validation error
+//     if (result.error) {
+//       return res.status(400).send(result.error.details[0].message);
+//     }
+
+//     // check if genre already exist in the database
+//     const genreExist = await Genre.exists({ name: req.body.name });
+
+//     if (genreExist) {
+//       return res.status(400).json({ message: "Genre already exists" });
+//     }
+
+//     const genre = new Genre(result.value);
+
+//     const savedGenre = await genre.save();
+
+//     res.status(201).json(savedGenre);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// });
 
 // PUT: Update existing genre in the genre list
 genreRoutes.put("/:id", async (req, res) => {
@@ -135,6 +159,7 @@ genreRoutes.delete("/:id", async (req, res) => {
 
 module.exports = {
   genreRoutes,
-  getSingleGenre,
   getAllGenres,
+  getSingleGenre,
+  createGenre,
 };
--- src/routes/genre.js
+++ src/routes/genre.js
@@ -2,11 +2,14 @@ const router = require("express").Router();
 const {
   getAllGenres,
   getSingleGenre,
+  createGenre,
   genreRoutes,
 } = require("../controllers/genresRouter");
 
 router.get("/", getAllGenres);
 
 router.get("/:id", getSingleGenre);
+
+router.post("/", createGenre);
 
 module.exports = router;
