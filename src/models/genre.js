7e2e80129c5e64ff977f40ed9058b90a4b3d73ed
ddoyediran <ddoyediran@gmail.com>
2023-03-31 18:10:46 +0200

    model updated

--- src/models/genre.js
+++ src/models/genre.js
@@ -14,12 +14,12 @@ const genreSchema = new mongoose.Schema({
 });
 
 // Transform the schema object
-genreSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    //returnedObject.id = returnedObject._id.toString();
-    //delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+// genreSchema.set("toJSON", {
+//   transform: (document, returnedObject) => {
+//     //returnedObject.id = returnedObject._id.toString();
+//     //delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
 
 module.exports = mongoose.model("Genre", genreSchema);
